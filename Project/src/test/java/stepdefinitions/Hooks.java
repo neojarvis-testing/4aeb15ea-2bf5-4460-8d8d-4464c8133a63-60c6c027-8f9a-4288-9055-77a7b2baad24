af448f460e05c104539d99f28e9964b62954e40e
kris733 <krishnavardhan733@gmail.com>
2025-04-25 10:40:28 +0000

    krishna first commit

---
+++ Project/src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,35 @@
+package stepdefinitions;
+ 
+import com.aventstack.extentreports.ExtentReports;
+ 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import utils.Base;
+import utils.Reporter;
+ 
+public class Hooks extends Base{
+    static ExtentReports report;
+    @BeforeAll
+    public static void generate(){
+        report =Reporter.createReport("Mayors_Report");
+ 
+    }
+ 
+    @Before
+    public void launch(){
+        openBrowser();
+    }
+ 
+    @After
+    public void close(){
+        driver.quit();
+    }
+ 
+    @AfterAll
+    public static void flush(){
+        report.flush();
+    }
+ 
+}
