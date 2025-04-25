9d82344357348870ccdff83264a27d82e4f9cb06
inturi123 <inturipranith2456@gmail.com>
2025-04-25 11:33:57 +0000

    Praneeth first commit

--- Project/src/test/java/stepdefinitions/HomeStepDefinition.java
+++ Project/src/test/java/stepdefinitions/HomeStepDefinition.java
@@ -1,5 +1,55 @@
 package stepdefinitions;
 
-public class HomeStepDefinition {
+import com.aventstack.extentreports.ExtentTest;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.HomePage;
+import utils.LoggerHandler;
+
+public class HomeStepDefinition {
+    LoggerHandler logs;
+    ExtentTest test;
+    HomePage rolex;
+@Given("I hover on Rolex")
+public void i_hover_on_rolex() {
+    logs = new LoggerHandler();
+    test = Hooks.reports.createTest("TestCase2");
+    rolex = new HomePage(test, logs);
+    rolex.clickOnCookies();
+    rolex.clickOnRolex();
+}
+@When("I click on deepsea")
+public void i_click_on_deepsea() {
+    rolex.deepSea();
+}
+@When("I verify the page title {string}")
+public void i_verify_the_page_title(String string) {
+    rolex.verifyDeepSea(string);
+}
+@When("I click on rolex watches")
+public void i_click_on_rolex_watches() {
+    rolex.clickOnRolexWatches();
+}
+@When("I verify the text {string}")
+public void i_verify_the_text(String string) {
+    rolex.verifyRolexWatch(string);
+}
+@When("I click on discover watches")
+public void i_click_on_discover_watches() {
+    rolex.clickOnDiscoverWatches();
+}
+@When("I click on New watches")
+public void i_click_on_new_watches() {
+    rolex.clickNewWatches();
+}
+@When("I click on watchmaking")
+public void i_click_on_watchmaking() {
+    rolex.clickWatchMaking();
+}
+@Then("I verify the keyword {string}")
+public void i_verify_the_keyword(String string) {
+    rolex.veirfyWatchMaking(string);
+}
 }
