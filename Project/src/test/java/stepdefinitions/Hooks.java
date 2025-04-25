7762c70e22649d58e511cdbe9ea62f6b4f65dd7a
pavanchennaboina5899 <pavankumar967691@gmail.com>
2025-04-25 10:54:32 +0000

    pavan commit

---
+++ Project/src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,41 @@
+package stepdefinitions;
+
+import com.aventstack.extentreports.ExtentReports;
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import utils.*;
+
+public class Hooks extends Base{
+    public static ExtentReports report;
+
+    @BeforeAll
+    public static void iniReport()
+    {
+        report = Reporter.createReport("Mayors_Report");
+    }
+
+    @Before
+    public void openBrow()
+    {
+        openBrowser();
+    }
+
+    @After
+    public void closeBrowser()
+    {
+        if(driver!=null)
+        {
+            driver.quit();
+        }
+    }
+
+    @AfterAll
+    public static void flushReport()
+    {
+        report.flush();
+    }
+
+}
--- Project/src/test/java/utils/Property.java
+++ Project/src/test/java/utils/Property.java
@@ -1,5 +1,31 @@
 package utils;
 
-public class Property {
+import java.io.FileInputStream;
+import java.util.Properties;
 
+public class Property {
+    public static FileInputStream file;
+    public static Properties prop=null;
+
+    /*Method name:loadBaseProperties
+    *Author:Sumayya Sultana
+    *Description:This method is for load configuration settings from an external properties file
+    *Return Type:Properties
+    */
+
+
+    public Properties loadBaseProperties(){
+        String propertiesPath = System.getProperty("user.dir") + "/config/browser.properties";
+        try {
+            file = new FileInputStream(propertiesPath);
+            prop = new Properties();
+            prop.load(file);
+            return prop;
+
+        } catch (Exception e) {
+            System.out.println("Can not load properties file "+e.getMessage());
+
+        }
+        return prop;
+    }
 }
